de229c3d54aafaab617a3cbe51a282fe0b4b730e
ganeshbandaru567 <99868339+ganeshbandaru567@users.noreply.github.com>
2022-02-24 15:15:24 +0530

    #include<stdio.h> int main() {     int n,a,b,c,sum=0,p=1;     scanf("%d",&n);     c=n;
        while(n!=0)
        {
            a=n%10;
            sum=sum+a;
            n=n/10;
        }
        while(c!=0)
        {
            b=c%10;
            p=p*b;
            c=c/10;
        }
        if(sum==p)
        {
        printf("Spy Number");
        }
        else
        {
            printf("Not Spy Number");
        }
    }

---
+++ Spy_Number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b,c,sum=0,p=1;
+    scanf("%d",&n);
+    c=n;
+    
+    while(n!=0)
+    {
+        a=n%10;
+        sum=sum+a;
+        n=n/10;
+    }
+    while(c!=0)
+    {
+        b=c%10;
+        p=p*b;
+        c=c/10;
+    }
+    if(sum==p)
+    {
+    printf("Spy Number");
+    }
+    else
+    {
+        printf("Not Spy Number");
+    }
+}
